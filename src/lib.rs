3cce442985c2c64750da9e2ecd0b7e859c1a06d9
Crystal Keck <Crystal.Keck@millerwelds.com>
2025-07-24 10:52:46 -0500

    Move clippy lints to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,16 @@
+//! I2C driver for the TI MCF8316C BLDC motor driver
+//!
+
+
 #![no_std]
+#![deny(missing_docs)]
+#![deny(warnings)]
+#![deny(missing_copy_implementations)]
+#![deny(missing_debug_implementations)]
+#![deny(clippy::missing_panics_doc)]
+#![deny(clippy::undocumented_unsafe_blocks)]
+#![deny(clippy::std_instead_of_core)]
+#![deny(clippy::fallible_impl_from)]
 
 pub mod protocol;
 pub mod registers;
