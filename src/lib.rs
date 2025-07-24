8094182c875cb8649b9fbcd8efca229cd79811de
Crystal <45602803+CrystalKeck27@users.noreply.github.com>
2025-07-24 16:00:18 +0000

    Crate Metadata (#2)
    * Add license
    
    * Add no_std marker and more package metadata
    
    * Move clippy lints to lib.rs
    
    * Remove new lints
    
    ---------
    
    Co-authored-by: Crystal Keck <Crystal.Keck@millerwelds.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+//! I2C driver for the TI MCF8316C BLDC motor driver
+//!
+
+
+#![no_std]
+#![deny(clippy::missing_panics_doc)]
+#![deny(clippy::undocumented_unsafe_blocks)]
+#![deny(clippy::std_instead_of_core)]
+#![deny(clippy::fallible_impl_from)]
+
 pub mod protocol;
 pub mod registers;
 
