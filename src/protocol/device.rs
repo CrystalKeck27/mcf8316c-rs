7bbe84e0f4cb7d0bdaac813cb3938dfa6d19a9fa
Crystal Keck <Crystal.Keck@millerwelds.com>
2025-08-01 14:21:33 -0500

    Remove Sized restriction

--- src/protocol/device.rs
+++ src/protocol/device.rs
@@ -126,7 +126,7 @@ impl<I2C: embedded_hal::i2c::I2c<SevenBitAddress>> MCF8316C<I2C> {
     }
 
     /// Writes data to the specified register and verifies the write by reading back the value.
-    pub fn write_verify<T: FlexibleRegister>(&mut self, data: &T) -> Result<(), ReadError<I2C::Error>> {
+    pub fn write_verify<T: FlexibleRegister + ?Sized>(&mut self, data: &T) -> Result<(), ReadError<I2C::Error>> {
         self.write_u32(data.address(), data.value())?;
         data.compare_internal(self.read_u32(data.address())?)
             .then_some(())
